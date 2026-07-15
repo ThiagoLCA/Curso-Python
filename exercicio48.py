b573756b6c8092ec9706a2aa3c88f59e1fd2a21f
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-15 18:01:40 +0000

    novocommit

--- exercicio48.py
+++ exercicio48.py
@@ -3,3 +3,20 @@
 # será adicionado. No finalserá exibido todos os valores unicos digitados
 # em ordem crescente
 
+numeros = list ()
+
+while True:
+    n= int (input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append (n)
+        print ('Valor adicionado com sucesso!....')
+    else:
+        print ('Valor Duplicado! Não vou adicionar....')
+    resposta = input('Quer continuar: [S/N]')
+    if resposta in 'Nn':
+        break
+
+    print ('-=' *30)
+    numeros.sort ()
+    print (f'Você digitou os valores: {numeros}')
+
--- exercicio50.py
+++ exercicio50.py
@@ -4,3 +4,14 @@
 # b - a lista de valores organizada de forma decrescente. 
 # c - se o valor 5 foi digitado e está ou não na lista
 
+numeros= list ()
+while True:
+    numeros.append (int (input('Digite um numero: ')))
+    resposta = str(input(' Quer Continuar? [S/N]'))
+    if resposta in 'Nn':
+        break
+
+print ('-='*30)
+
+print (f'Você digitou {len(numeros)} Elementos')
+numeros
