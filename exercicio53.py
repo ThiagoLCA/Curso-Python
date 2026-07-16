5f7ea819f0be2f84e4037f81b0907c1e79b48d83
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-16 19:14:24 +0000

    novocommit

---
+++ exercicio53.py
@@ -0,0 +1,6 @@
+# Exemplo de Lista Composta - Lista dentro de Lista
+
+
+galera = [['João', 11], ['Livia', 40], ['Thiago', 39],['Fernanda', 27]]
+for p in galera:
+    print (f'{p[0]} tem {p[1]} anos de idade.')
