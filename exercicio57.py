40b8f3efd2031ee7208560d2f0502a6066eea6b3
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-19 17:50:02 +0000

    novocommit

--- exercicio57.py
+++ exercicio57.py
@@ -1,3 +1,12 @@
 # Crie um programa que crie uma matriz de dimensão 3x3 e preencha com 
 # valores lidos pelo teclado
 
+matriz = [[0,0,0], [0,0,0], [0,0,0]]
+for l in range (0,3):
+    for c in range (0,3):
+        matriz [l] [c] = int (input ('Digite um número para [{l}, {c}]:  '))
+print ('-=' * 30)
+for l in range (0,3):
+    for c in range (0,3):
+        print (f'[{matriz [l][c]:^5}]', end= '')
+    print ()
