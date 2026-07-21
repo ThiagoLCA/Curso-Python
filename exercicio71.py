5364d424eb8f6fa1f0396411cb131f036dbefaff
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-21 15:57:54 +0000

    novocommit

--- exercicio71.py
+++ exercicio71.py
@@ -3,4 +3,18 @@
 # literal indicando se uma pessoa tem voto negado, opcional ou 
 # obrigatório nas eleições
 
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade <16:
+        return f'com {idade} anos:NÂO VOTA'
+    elif 16<=idade <18 or idade >65:
+        return f'com {idade} anos: VOTO OPCIONAL'
+    else:
+        return f'com {idade} anos: VOTO OBRIGATÓRIO'
+    
 
+# Programa principal
+nasc= int(input('Em que ano você nasceu?:  '))
+print (voto(nasc))
--- exercicio72.py
+++ exercicio72.py
@@ -3,3 +3,24 @@
 # que será um valor lógico (opcional) indicando se será mostrado ou não
 # na tela o processo de cálculo do fatorial
 
+def fatorial (n, show= False):
+    """
+    -> Calcula o fatorial de um número.
+    :Paramametro n = o numero a ser calculado.
+    :parametro show: (Opcional), mostrar ou não a conta.
+    :return: Retorna o valor do fatorial de um numero n.
+    """
+    f= 1
+    for c in range (n,0,-1):
+        if show:
+            print (c, end= '')
+            if c > 1:
+                print (' x ', end= '')
+            else:
+                print (' = ', end= '')
+        f*=c
+    return f
+
+#Programa principal
+print (fatorial (5, show= True))
+help(fatorial)
