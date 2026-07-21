ed4bf434c297cd15151665940eae97c8e97354e3
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-21 16:58:54 +0000

    novocommit

--- exercicio74.py
+++ exercicio74.py
@@ -1,3 +1,21 @@
 # desafio 104 - Crie um programa com a função leiaint(), que vai funcionar de forma 
 # semelhante a função input(), do python. Só que fazendo a validação
 # para aceitar apenas um valor numerico. ex: n= leiaint('digite um n')
+
+def leiaInt(msg):
+    ok= False
+    valor = 0
+    while True:
+        n= str(input(msg))
+        if n.isnumeric():
+            valor = int (n)
+            ok= True
+        else:
+            print('\033[0;31mERRO! Digite um numero inteiro valido.\033[m')
+        if ok:
+            break
+    return valor
+
+#Programa Principal
+n= leiaInt ('Digite um numero: ')
+print (f'Você acabou de digitar o numero {n}')
