e24d588594f339ef88bc495a453c53ee525e5761
ThiagoLCA <thiagolcaraujo87@gmail.com>
2026-07-21 16:26:56 +0000

    novocommit

--- exercicio73.py
+++ exercicio73.py
@@ -3,3 +3,19 @@
 # marcou. O programa deverá ser capaz de mostrar a ficha do jogador,
 # mesmo que algum dado não tenha sido informado corretamente.
 
+def ficha (jog= '<desconhecido>', gol= 0):
+    print (f'O Jogador {jog} fez {gol} gol(s) no campeonato')
+
+
+#Programa principal
+n= str(input('Nome do Jogador: '))
+g= str(input('numero de gols: '))
+if g.isnumeric():
+    g= int(g)
+else:
+    g=0
+if n.strip() == '':
+    ficha (gol=g)
+else:
+    ficha (n,g)
+    
